aef2b100f6a2241ab433057ed126c938e9e7bb85
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-06 12:33:02 +0530

    connected git

---
+++ src/main/java/BASICJAVA/Project001/App.java
@@ -0,0 +1,13 @@
+package BASICJAVA.Project001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
